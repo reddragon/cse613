e0cd5fc8bc18164252a53cd0feeb815c1eeff3d1
Gaurav Menghani <gaurav@login1.ls4.tacc.utexas.edu>
2012-03-16 19:23:01 -0500

    Cilkview job scripts

---
+++ hw1/Prob-1/e/cilkview_base32_inp32768.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#$ -V
+#$ -cwd
+#$ -q development
+#$ -pe 12way 24
+#$ -N Cilkview_32768_Development
+#$ -o output-$JOB_NAME
+#$ -e error-$JOB_NAME
+#$ -M gaurav.menghani@gmail.com
+#$ -m be
+#$ -l h_rt=01:00:00
+
+cilkview -trials all ./a.out 12 32 ~/hw1/hw1/samples/turn-in/rand-32768-in.txt
