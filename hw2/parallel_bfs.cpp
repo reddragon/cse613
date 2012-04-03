eddb601b7b2041596aae26fe23a081ee9d420497
Dhruv <dhruvbird@gmail.com>
2012-04-03 06:28:10 -0700

    Add code for part-(h)

--- hw2/parallel_bfs.cpp
+++ hw2/parallel_bfs.cpp
@@ -17,6 +17,8 @@ typedef map<int, int> matrix_1d_t;
 typedef vector<matrix_1d_t> matrix_2d_t;
 
 #define cfor cilk_for
+#define MAX_STEAL_ATTEMPTS (2*p)
+#define MIN_STEALABLE_QSIZE 32
 
 struct node_pair {
     int p, u;
@@ -61,6 +63,8 @@ struct segment_t {
     { }
 };
 
+typedef vector<segment_t> vseg_t;
+
 graph_t graph;
 vi_t d, sources;
 int n, m, r;
@@ -79,6 +83,11 @@ int QminNonEmpty = 0;
 
 int infinity = -1;
 cilk::mutex ns_mutex;
+
+#if defined PARTH
+vseg_t Qsegments;
+vector<cilk::mutex> QsegMutexes;
+#endif
 
 #if defined PARTD
 vi_t parent;
@@ -136,6 +145,74 @@ next_segment(int size) {
     return seg;
 }
 
+
+#if defined PARTH
+// With work stealing
+void
+parallel_bfs_thread(int k, int) {
+    while (true) {
+        QsegMutexes[k].lock();
+        segment_t *pseg = &(Qsegments[k]);
+        while (pseg->i != pseg->j) {
+            int index = pseg->i++;
+            QsegMutexes[k].unlock();
+            
+            node_pair np = (*(pseg->q))[index];
+            int u = np.u;
+            
+            for (matrix_1d_t::iterator i = graph.impl[u].begin(); 
+                 i != graph.impl[u].end(); ++i) {
+                int v = i->first;
+                // assert(false);
+                // fprintf(stderr, "processing edge (%d, %d), d[%d] = %d, d[%d] = %d\n", u, v, u, d[u], v, d[v]);
+                
+                if (d[v] == infinity) {
+                    d[v] = d[u] + 1;
+                    pmax[k] = std::max(pmax[k], d[v]);
+                    Qout[k].push_back(node_pair(u, v));
+                }
+            }
+
+            QsegMutexes[k].lock();
+
+        } // while()
+
+        QsegMutexes[k].unlock();
+
+        segment_t tmp(NULL, 0, 0);
+        int steal_attempts;
+        for (steal_attempts = 0; steal_attempts < MAX_STEAL_ATTEMPTS; ++steal_attempts) {
+            int rk = rand() % p;
+            if (rk != k) {
+                QsegMutexes[rk].lock();
+                // Steal half of the nodes if there are >= MIN_STEALABLE_QSIZE nodes.
+                tmp = Qsegments[rk];
+                if (tmp.j - tmp.i >= MIN_STEALABLE_QSIZE) {
+                    int mid = tmp.i + (tmp.j - tmp.i)/2;
+
+                    fprintf(stderr, "stole %d nodes from queue %d\n", (tmp.j - tmp.i)/2, rk);
+
+                    Qsegments[rk].j = mid + 1;
+                    tmp.i = mid;
+                    QsegMutexes[rk].unlock();
+                    break;
+                }
+                QsegMutexes[rk].unlock();
+            }
+        }
+        if (steal_attempts == MAX_STEAL_ATTEMPTS) {
+            break;
+        } else {
+            QsegMutexes[k].lock();
+            Qsegments[k] = tmp;
+            QsegMutexes[k].unlock();
+        }
+
+    } // while (true)
+    QminNonEmpty = Qin.size();
+
+}
+#else
 void
 parallel_bfs_thread(int k, int size) {
     segment_t seg = next_segment(size);
@@ -172,9 +249,11 @@ parallel_bfs_thread(int k, int size) {
         seg = next_segment(size);
     }
 }
+#endif
 
 int
 sizeofQin() {
+    // TODO - Use cilk_for with reducers
     int sz = 0;
     for (int i = 0; i < (int)Qin.size(); ++i) {
         sz += Qin[i].size();
@@ -213,6 +292,13 @@ parallel_bfs(int s) {
     Qin[0].push_back(node_pair(-1, s));
     int nseg = p; // TODO - FIXME
 
+#if defined PARTH
+    Qsegments.clear();
+    Qsegments.resize(p, segment_t(NULL, 0, 0));
+    Qsegments[0] = segment_t(&(Qin[0]), 0, 1);
+    QsegMutexes.resize(p);
+#endif
+
     // fprintf(stderr, "QminNonEmpty: %d, Qin.size(): %d\n", QminNonEmpty, Qin.size());
     while (QminNonEmpty < (int)Qin.size()) {
         int size = ceil((double)sizeofQin() / nseg);
@@ -230,6 +316,13 @@ parallel_bfs(int s) {
         Qout.resize(p);
         QinStart.clear();
         QinStart.resize(p, 0);
+
+        cfor (int i = 0; i < p; ++i) {
+#if defined PARTH
+            Qsegments[i] = segment_t(&(Qin[i]), 0, Qin[i].size());
+#endif
+        }
+
         // fprintf(stderr, "QminNonEmpty: %d, Qin.size(): %d\n", QminNonEmpty, Qin.size());
     }
 
