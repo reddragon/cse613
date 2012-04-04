907c98c703bd961c6fb407ff9298da7d0d9509ae
Gaurav Menghani <gaurav@login2.ls4.tacc.utexas.edu>
2012-04-03 19:42:51 -0500

    Add queue submission scripts and update makefile

---
+++ hw2/qrun-sample02-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#$ -V
+#$ -cwd
+#$ -q development
+#$ -pe 12way 24
+#$ -N parallel_bfs_sample02
+#$ -o output-$JOB_NAME
+#$ -e error-$JOB_NAME
+#$ -M dhruvbird@gmail.com
+#$ -m be
+#$ -l h_rt=01:00:00
+
+HW2='/work/01905/rezaul/CSE613/HW2/samples/'
+time ./serial < $HW2/sample-02-in.txt > samples/serial-02.out
+time ./parallel < $HW2/sample-02-in.txt > samples/parallel-02.out
+time ./parallel_d < $HW2/sample-02-in.txt > samples/parallel_d-02.out
+time ./parallel_h < $HW2/sample-02-in.txt > samples/parallel_h-02.out
