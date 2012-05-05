2dad5294a03f2a5c61299109e43456c3747b0153
Gaurav Menghani <gaurav@login2.ls4.tacc.utexas.edu>
2012-05-05 13:07:37 -0500

    random_shuffle instead of generating

--- hw3/task-2/rand.cpp
+++ hw3/task-2/rand.cpp
@@ -1,18 +1,18 @@
 #include<cstdlib>
 #include<set>
 #include<iostream>
+#include<vector>
 
 int main(int argc, char **argv) {
     int n = atoi(argv[1]);
-    std::set<int> s;
-    srand(0);
+    std::vector<int> v;
+    for (int i = 0; i < n; i++) {
+        v.push_back(i);
+    }
+    random_shuffle(v.begin(), v.end());
     std::cout << n << std::endl;
     for (int i = 0; i < n; i++) {
-        int x;
-        do {
-            x = rand() % (1<<20);
-        } while (s.find(x) != s.end());
-        s.insert(x);
-        std::cout << x << std::endl;
+        std::cout << v[i] << std::endl;
     }
+    std::cout << std::endl;
 }
