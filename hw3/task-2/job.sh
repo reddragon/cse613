66cd10773a4786b916a99988db8410b7cab6de0a
Gaurav Menghani <gaurav@login2.ls4.tacc.utexas.edu>
2012-05-05 13:29:03 -0500

    Experimenting with q in the job script

--- hw3/task-2/job.sh
+++ hw3/task-2/job.sh
@@ -11,4 +11,11 @@
 #$ -m be
 #$ -l h_rt=01:00:00
 
-ibrun ./distributed_sort-mpi < test.in
+ibrun ./distributed_sort-mpi test.in 4 >> t.out
+ibrun ./distributed_sort-mpi test.in 8 >> t.out
+ibrun ./distributed_sort-mpi test.in 16 >> t.out
+ibrun ./distributed_sort-mpi test.in 32 >> t.out
+ibrun ./distributed_sort-mpi test.in 64 >> t.out
+ibrun ./distributed_sort-mpi test.in 128 >> t.out
+ibrun ./distributed_sort-mpi test.in 256 >> t.out
+ibrun ./distributed_sort-mpi test.in 512 >> t.out
